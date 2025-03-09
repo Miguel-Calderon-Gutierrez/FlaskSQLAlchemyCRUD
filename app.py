88c42e80b0307cfa14780e4b443181777b14fe64
Miguel Calderon <miguelcalderon.dev@gmail.com>
2025-03-09 00:28:47 -0500

    Conexion a la db

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask
 from routes.contacts import contacts
-
+from utils.db import db  
 
 app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://root:1234@localhost/contactsdb"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db.init_app(app)
 app.register_blueprint(contacts)
-
-
--- routes/contacts.py
+++ routes/contacts.py
@@ -1,11 +1,28 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
-contacts = Blueprint('Contacts',__name__)
+contacts = Blueprint("Contacts", __name__)
+
 
 @contacts.route("/")
 def home():
-    return "Contacts list"   
+    return render_template("index.html")
+
 
 @contacts.route("/new")
-def addContact():
-    return "Saving a contact"   
+def add():
+    return render_template("new.html")
+
+
+@contacts.route("/update")
+def update():
+    return "update contact"
+
+
+@contacts.route("/delete")
+def delete():
+    return "delete contact"
+
+
+@contacts.route("/about")
+def about():
+    return render_template("about.html")
